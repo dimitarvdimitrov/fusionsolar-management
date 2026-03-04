a92d24dc70502249f1693685fd51ab8f656fced1
Dimitar Dimitrov <hi@dimitar.dev>
2026-03-04 19:47:44 +0000

    Use higher LOW_POWER_SETTING

--- config.py
+++ config.py
@@ -120,7 +120,7 @@ FUSIONSOLAR_PASSWORD = get_config_value("FUSIONSOLAR_PASSWORD")
 PRICE_THRESHOLD = float(get_config_value("PRICE_THRESHOLD", "15.04"))
 
 # Power settings (in kW)
-LOW_POWER_SETTING = get_config_value("LOW_POWER_SETTING", "5.000") # Power limit when prices are high
+LOW_POWER_SETTING = get_config_value("LOW_POWER_SETTING", "7.000") # Power limit when prices are high
 HIGH_POWER_SETTING = get_config_value("HIGH_POWER_SETTING", "no limit")   # Power limit when prices are low
 
 # Telegram configuration constants
